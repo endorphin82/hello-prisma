fd5ab590ba0345e575101fcce1f321b9c2e4cedd
endorphin82 <endorphin82@gmail.com>
2020-05-29 17:34:09 +0300

    add migration

--- index.ts
+++ index.ts
@@ -1,43 +1,83 @@
-import {PrismaClient} from "@prisma/client"
+import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
 async function main() {
-
 /*
-    await prisma.user.create({
-        data: {
-            name: "Alice",
-            email: "alice@prisma.io",
-            posts: {
-                create: { title: "2 Hello World" },
-            },
-            profile: {
-                create: { bio:  "I like turtles" }
-            }
-        }
-    })
+  await prisma.user.create({
+    data: {
+      name: 'Alice',
+      email: 'alice@prisma.io',
+      posts: {
+        create: { title: '2 Hello World' }
+      },
+      profile: {
+        create: { bio: 'I like turtles' }
+      }
+    }
+  })
 */
 /*
-    const post = await prisma.post.update({
-        where: { id: 1 },
-        data: { published: true },
+  const post = await prisma.post.update({
+    where: { id: 1 },
+    data: { published: true }
+  })
+  console.log(post)
+*/
+/*
+  const filteredPosts = await prisma.post.findMany({
+    where: {
+      OR: [
+        { title: { contains: 'hello' } },
+        { content: { contains: 'hello' } }
+
+    }
+  })
+  console.dir(feredPosts, { depth: null })
+*/
+/*
+  const post = await prisma.post.create({
+    data: {
+      title: 'Join us for Prisma Day 2020',
+      author: {
+        connect: { email: 'alice@prisma.io' }
+      }
+    }
+  })
+*/
+/*
+  const posts = await prisma.profile
+    .findOne({
+      where: { id: 1 }
     })
-    console.log(post)
-    */
-    const allUsers = await prisma.user.findMany({
-        include: {
-            posts: true,
-            profile: true
-        },
-    })
-    console.dir(allUsers, {depth: null})
+    .user()
+    .posts()
+
+  console.log(posts)
+*/
+/*
+  const deletedUser = await prisma.user.delete({
+    where: { email: 'sarah@prisma.io' }
+  })
+
+  console.log(deletedUser)
+*/
+
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true
+    }
+  })
+  console.dir(allUsers, { depth: null })
+
 }
 
+
 main()
-    .catch(e => {
-        throw e
-    })
-    .finally(async () => {
-        await prisma.disconnect()
-    })
+  .catch(e => {
+    throw e
+  })
+  .finally(async () => {
+    await prisma.disconnect()
+  })
